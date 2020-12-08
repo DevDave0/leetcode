c385c31bb6486468fbc102e52ab4bb4547f60113
devdave0 <dbchung24@gmail.com>
2020-12-07 21:33:13 -0500

    contains duplicate

---
+++ contains-duplicate.js
@@ -0,0 +1,13 @@
+const containsDuplicate = (nums) => {
+    hash = {};
+    for (let i = 0; i < nums.length; i++){
+        if (hash[nums[i]]){
+            return true
+        }
+        else {
+            hash[nums[i]] = 1
+        }
+    }
+    return false
+
+};
