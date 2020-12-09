f14349f34af09c9b1cce17d04b2245b158eb2c9b
devdave0 <dbchung24@gmail.com>
2020-12-09 08:55:34 -0500

    product of array except self

---
+++ product-of-array-except-self.js
@@ -0,0 +1,15 @@
+const productExceptSelf = (nums) => {
+    let result = [];
+    let productSoFar = 1;
+    for (let i = 0; i < nums.length; i++){
+        result[i] = productSoFar
+        productSoFar *= nums[i]
+    }
+    productSoFar = 1 
+    for (let j = nums.length - 1; j >= 0; j--){
+        result[j] *= productSoFar
+        productSoFar *= nums[j]
+    }
+    return result
+
+};
