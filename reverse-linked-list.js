68eb43419d482d9ebd1800f028b100aa6dc73d3c
devdave0 <dbchung24@gmail.com>
2020-12-17 17:36:20 -0500

    reverse linked list

---
+++ reverse-linked-list.js
@@ -0,0 +1,10 @@
+const reverseList = (head) => {
+    let pre = null;
+    while(head){
+        const next = head.next
+        head.next = pre
+        pre = head 
+        head = next
+    }
+    return pre
+};
