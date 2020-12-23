a7951158e2b36066adb153bfac82927be0bf3e5a
devdave0 <dbchung24@gmail.com>
2020-12-22 20:03:54 -0500

    minimum add to make parentheses valid

---
+++ minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,19 @@
+const minAddToMakeValid = (S) =>{
+    let left = 0;
+    let right = 0;
+    
+    for(let i = 0; i < S.length; i++) {
+        if(S.charAt(i) == '(') {
+            left++;
+        } else{
+            if(left > 0) {
+                left--;
+            } else{
+                right++;
+            }
+        }
+    }
+    
+    return left + right;
+    
+};
