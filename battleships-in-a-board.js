ae19bdf5f13afa2c756f7c1048c05b90661ecc2d
devdave0 <dbchung24@gmail.com>
2020-12-29 17:35:07 -0500

    battleships in a board

---
+++ battleships-in-a-board.js
@@ -0,0 +1,14 @@
+const countBattleships = (board) => {
+    let count = 0;
+    for (let i = 0; i < board.length; i++){
+        for (let j = 0; j < board[i].length; j++){
+            if (board[i][j] === "X"){
+                if (i>0 && board[i-1][j] == "X" || j > 0 && board[i][j-1] == "X"){
+                    continue;
+                }
+                count++;
+            }
+        }
+    }
+    return count;
+};
